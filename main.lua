c5f2002532e45624830620c9cf0a9cc1e7d3b838
Tx Mat <mathieu.ponal@gmail.com>
2023-09-14 00:30:48 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,79 @@
+-- PID Controller for Mekanism Fission Reactor
+-- Author: Tx Mat
+
+-- Reactor
+local r = peripheral.wrap("back")
+
+-- Constantes PID (vous pouvez ajuster ces valeurs)
+local kp = 0.05  -- Terme proportionnel
+local ki = 0.002 -- Terme intégral
+local kd = 0.02 -- Terme dérivés
+local deadband = 1.0 -- Plage d'erreur acceptable autour de la valeur cible
+
+-- Variables
+local targetEfficiency = 100.0 -- Valeur cible d'efficacité
+local integral = 0.0 -- Variable pour la composante intégrale
+local lastError = 0.0 -- Variable pour stocker la dernière erreur
+
+local sign = 1
+local lastEfficiency = 0.0
+local currentEfficiency = 0.0
+
+function getCurrentEfficiency()
+    r.getEfficiency()
+end
+
+
+function addIncrement(inc)
+    r.adjustReactivity(inc)
+end
+
+-- Fonction de contrôle PID
+function pidControl()
+    lastEfficiency = currentEfficiency
+    currentEfficiency = getCurrentEfficiency()
+    local error = targetEfficiency - currentEfficiency -- Calcul de l'erreur
+    if math.abs(error) > deadband then
+
+        -- Terme proportionnel
+        local p = kp * error
+
+        -- Terme intégral
+        integral = integral + ki * error
+
+        -- Terme dérivé
+        local derivative = kd * (error - lastError)
+        lastError = error
+
+        -- Calcul de la commande totale
+        local controlOutput = p + integral + derivative
+
+        if currentEfficiency < lastEfficiency then
+            sign = sign * -1
+        end
+
+        controlOutput = sign * controlOutput
+
+        -- Utilisez la commande pour ajuster la Current Reactivity
+        addIncrement(controlOutput)
+
+        print("PID Control: " .. controlOutput .. " ERR: " .. error .. " IR: " .. integral .. " DR: " .. derivative)
+        print("Current Efficiency: " .. currentEfficiency)
+
+        -- Répétez le contrôle à intervalles réguliers
+        os.sleep(5) -- 5 secondes
+    else
+        local integral = 0.0 -- Variable pour la composante intégrale
+        local lastError = 0.0 -- Variable pour stocker la dernière erreur
+
+        local sign = 1
+        local lastEfficiency = 0.0
+        local currentEfficiency = 0.0
+        print("# Target Reactivity Changed !#")
+    end
+end
+
+-- Boucle de contrôle principale
+while true do
+    pidControl()
+end
